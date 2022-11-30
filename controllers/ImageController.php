7689bccc5b5c89fe03c00c1a269d2b6616dcb21d
Арсений Сендецкий <ars_121229@mail.ru>
2022-12-01 03:42:14 +0800

    crug2 сделан

--- controllers/ImageController.php
+++ controllers/ImageController.php
@@ -1,21 +1,21 @@
 <?php
 
-class ObjectController extends TwigBaseController {
-    public $template = "object.twig";
+class ImageController extends TwigBaseController {
+    public $template = "object_image.twig";
 
     public function getContext(): array
     {
         $context = parent::getContext();
         
-        $query = $this->pdo->prepare("SELECT * FROM videocards WHERE id= :my_id;");
+        $query = $this->pdo->prepare("SELECT image, id FROM videocards WHERE id= :my_id;");
         $query->bindValue("my_id", $this->params['id']);
         $query->execute();
 
         $data = $query->fetch();
-        $context['description'] = $data['description'];
+
         $context['image'] = $data['image'];
-        $context['info_full'] = nl2br($data['info_full']);
         $context['id_v'] = $data['id'];
+ 
         return $context;
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,13 +4,9 @@ require_once '../framework/autoload.php';
 require_once '../controllers/MainController.php';
 require_once "../controllers/ObjectController.php";
 
-require_once '../controllers/ThirtyController.php';
-require_once '../controllers/ThirtyImageController.php';
-require_once '../controllers/ThirtyInfoController.php';
+require_once '../controllers/ImageController.php';
+require_once '../controllers/InfoController.php';
 require_once '../controllers/Controller404.php';
-require_once '../controllers/FourtyController.php';
-require_once '../controllers/FourtyImageController.php';
-require_once '../controllers/FourtyInfoController.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('../views');
 $twig = new \Twig\Environment($loader, [
@@ -28,5 +24,7 @@ $pdo = new PDO("mysql:host=localhost;dbname=gpus;charset=utf8", "root", "");
 $router = new Router($twig, $pdo);
 $router->add("/", MainController::class);
 $router->add("/videocards/(?P<id>\d+)", ObjectController::class);
+$router->add("/videocards/(?P<id>\d+)/info", InfoController::class);
+$router->add("/videocards/(?P<id>\d+)/image", ImageController::class);
 
 $router->get_or_default(Controller404::class);
